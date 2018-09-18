91104faa1a0080e65b4be4e4011e28c94070c7cb
Philip S(D) <philipsteininger@gmail.com>
2018-09-17 17:23:44 -0700

    added coords to boards

--- bship.js
+++ bship.js
@@ -1,12 +1,20 @@
 var rows = 10;
 var cols = 10;
 var board = [];
-for (var x = 0; x < rows; x++) {
+for (var x = 1; x <= rows; x++) {
     board[x] = Array(rows);
-    for (var y = 0; y < cols; y++) {
-        board[x][y] = "0";
+    for (var y = 1; y <= cols; y++) {
+        board[x][y] = " ";
     }
 }
+board.forEach((e, i) =>  {
+    if(i<10) {
+        e[0] = " " + i;
+    } else {
+        e[0] = "" + i;
+    }
+});
+board[0] = ["  ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 //var board = Array(cols).fill(new Array(rows).fill(0));
 var Boat = function (length, abbr, orientation) {
     this.length = length;
@@ -35,5 +43,18 @@ var placeShip = function (x, y, ship, orientation) {
         }
     }
 }
-placeShip(5, 5, aircraftCarrier, 0);
+placeShip(5, 5, aircraftCarrier, 1);
+console.log(board);
+
+var makeGuess = function (x, y) {
+    if (board[x][y] === " ") {
+        board[x][y] = ".";
+        return false;
+    } else {
+        board[x][y] = "X";
+        return true;
+    }
+}
+
+makeGuess(5,5);
 console.log(board);
