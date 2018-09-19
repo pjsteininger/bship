c767bd837d78392aa980f78fce1abc0a0b11e764
Philip S(D) <philipsteininger@gmail.com>
2018-09-19 09:04:32 -0700

    boats placed

--- bship.js
+++ bship.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-var col = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-var row = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+var colArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+var rowArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 /****************
  *              *
  * CONSTRUCTORS *
@@ -14,26 +14,34 @@ var Player = function (name, board, boats) {
 
 var Board = function (rows, cols) {
     this.board = [];
-    for (var x = 1; x <= rows; x++) {
-        this.board[x] = Array(rows);
-        for (var y = 1; y <= cols; y++) {
-            this.board[x][y] = "  ";
+    for (var row = 1; row <= rows; row++) {
+        this.board[row] = Array(rows);
+        for (var col = 1; col <= cols; col++) {
+            this.board[row][col] = "  ";
         }
     }
     this.board.forEach((e, i) => {
-        e[0] = " " + row[i - 1];
+        e[0] = " " + rowArr[i - 1];
     });
     this.board[0] = ["  ", " 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10"];
 }
 
-var Boat = function (length, name, abbr, orientation) {
+var Boat = function (length, name, abbr, orientation, coords) {
     this.length = length;
     this.name = name;
     this.abbr = abbr;
     if (orientation) {
         this.orientation = orientation;
     } else {
-        this.orientation = 0;
+        this.orientation = "0";
+    }
+    if (coords) {
+        this.coords = coords;
+    } else {
+        this.coords = {
+            row: null,
+            col: null
+        };
     }
     this.isSunk = false;
 
@@ -44,15 +52,67 @@ var Boat = function (length, name, abbr, orientation) {
  *  GAME FUNCTIONS  *
  *                  *
  * *****************/
-
-var placeShip = function (board, x, y, ship, orientation) {
-    if (orientation) {
-        for (var j = y; j < y + ship.length; j++) {
-            board[x][j] = ship.abbr;
+var checkSpace = function (board, row, col, boat, orientation) {
+    if ((orientation !== "1") && (orientation !== "0")) {
+        return false;
+    }
+    if (colArr.indexOf(col) === -1) {
+        return false;
+    } else {
+        col = colArr.indexOf(col) + 1;
+    }
+    if (rowArr.indexOf(row) === -1) {
+        return false;
+    } else {
+        row = rowArr.indexOf(row) + 1;
+    }
+    if (orientation === "0") {
+        if (col + boat.length > 10) {
+            return false;
         }
     } else {
-        for (var i = x; i < x + ship.length; i++) {
-            board[i][y] = ship.abbr;
+        if (row + boat.length > 10) {
+            return false;
+        }
+    }
+
+    if (board[row][col] === "  ") {
+        if (orientation === "1") {
+            for (var l = 1; l < boat.length; l++) {
+                if (board[row + l][col] === "  ") {
+                    continue
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            for (var l = 1; l < boat.length; l++) {
+                if (board[row][col + l] === "  ") {
+                    continue
+                } else {
+                    return false;
+                }
+            }
+        }
+
+    } else {
+        return false;
+    }
+    return true;
+}
+
+
+
+var placeShip = function (board, row, col, boat, orientation) {
+    col = colArr.indexOf(col) + 1;
+    row = rowArr.indexOf(row) + 1;
+    if (orientation === "1") {
+        for (var j = row; j < row + boat.length; j++) {
+            board[j][col] = boat.abbr;
+        }
+    } else {
+        for (var i = col; i < col + boat.length; i++) {
+            board[row][i] = boat.abbr;
         }
     }
 }
@@ -68,64 +128,60 @@ var makeGuess = function (board, x, y) {
 }
 
 var newGame = function (name) {
-    var boats = {
-        aircraftCarrier: new Boat(5, "Aircraft Carrier", "A"),
-        battleship: new Boat(4, "Battleship", "B"),
-        submarine: new Boat(3, "Submarine", "S"),
-        cruiser: new Boat(3, "Cruiser", "C"),
-        destroyer: new Boat(2, "Destroyer", "D")
-    }
+    var boats = [
+        new Boat(5, "Aircraft Carrier", " A"),
+        new Boat(4, "Battleship", " B"),
+        new Boat(3, "Submarine", " S"),
+        new Boat(3, "Cruiser", " C"),
+        new Boat(2, "Destroyer", " D")
+    ]
     var player = new Player(name, new Board(10, 10), boats);
     var cpu = new Player("cpu", new Board(10, 10), boats);
-    console.log(player.board);
-    console.log("player.boats[1]", player.boats[Object.keys(player.boats)[1]]);
-    for (boat in player.boats) {
-        placeABoat(player, boat);
-    }
+    //placeShip(player.board.board, 5, 5, boats[0], 0)
+
+
+    //     checkSpace(player.board.board, "0", "a", boats[0], 0),
+    //     checkSpace(player.board.board, "10", "a", boats[0], 0),
+    //     checkSpace(player.board.board, "10", "a", boats[0], 1)
+    // );
+    // console.log(player.board.board)
+    promptBoat(player, boats, 0);
+
+
     //TODO: Place cpu boats randomly
     //TODO: inquire where they want to put their ships
     //TODO: while all players or all cpu boats isSunk : false, {playerTurn(); cpuTurn()}
 }
 
-var placeABoat = function (player, boat) {
-    //for (boat in player.boats) {
-    console.log(player.board);
-    inquirer.prompt([{
-        type: "input",
-        name: "xcoords",
-        message: "Pick a row a-j for your " + boat.name + "?",
-    }, {
-        type: "input",
-        name: "ycoords",
-        message: "Pick a column 1-10 for your " + boat.name + "?",
-    }, {
-        type: "input",
-        name: "orientation",
-        message: "Place the ship horizontally (0) or vertically (1)?"
-    }]).then(answer => {
-        if ((row.includes(answer.xcoords)) && (col.includes(answer.ycoords)) && ((answer.orientation == 1) || answer.orientation == 0)) {
-            //TODO: place the boat on the board
-            console.log(answer);
-            //placeABoat(player, **nextBoat**)
-        } else {
-            console.log("Please input valid values");
-            placeABoat(player, boat);
-        }
-
-    });
-
-
-    // {
-
-
-    // },
-    // {
-
-    // },
-    // {
-
-    // },
-    //}
+var promptBoat = function (player, boats, i) {
+    if (i < boats.length) {
+        console.log(player.board);
+        inquirer.prompt([{
+            type: "input",
+            name: "row",
+            message: "Pick a row a-j for your " + boats[i].name + "?",
+        }, {
+            type: "input",
+            name: "col",
+            message: "Pick a column 1-10 for your " + boats[i].name + "?",
+        }, {
+            type: "input",
+            name: "orientation",
+            message: "Place the ship horizontally (0) or vertically (1)?"
+        }]).then(function (answer) {
+            if (checkSpace(player.board.board, answer.row, answer.col, boats[i], answer.orientation)) {
+                placeShip(player.board.board, answer.row, answer.col, boats[i], answer.orientation);
+                i++;
+                promptBoat(player, boats, i);
+            } else {
+                console.log("Please input valid values");
+                promptBoat(player, boats, i);
+            }
+        });
+    } else {
+        console.log(player.board);
+        
+    }
 }
 
 var playerTurn = function () {
