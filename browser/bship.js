f2fadb0163bc916d5cfff2860f76b9941687a1b3
Philip S(D) <philipsteininger@gmail.com>
2018-09-24 17:27:09 -0700

    firebase

--- browser/bship.js
+++ browser/bship.js
@@ -101,6 +101,31 @@ $(document).ready(function () {
     $(".msg-box").text("your moves | cpu moves");
     $("#game").append(generateBoard(10, 10));
     var btn = $("<button type='button' class='btn btn-primary'>").text("button");
+    var config = {
+        apiKey: "AIzaSyCcrKJgxnJp0SkukTc1HhI-dkxMfX3jT2s",
+        authDomain: "battleship-167f9.firebaseapp.com",
+        databaseURL: "https://battleship-167f9.firebaseio.com",
+        projectId: "battleship-167f9",
+        storageBucket: "battleship-167f9.appspot.com",
+        messagingSenderId: "167042498206"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+    
+    var sendGuess = function(coord) {
+        database.ref("player-guess").push({
+            coord: coord,
+            timestamp: firebase.database.ServerValue.TIMESTAMP
+        });
+    }
+
+    
+    
+    
+    
+    
+    
+    
     btn.on("click", function () {
         boat = !boat;
     });
